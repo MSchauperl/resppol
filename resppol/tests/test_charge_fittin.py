8935ed4c02cb8990ad2db5163f94083e8e9b0f58
MSchauperl <schauperl.michael@gmail.com>
2019-05-06 13:41:49 -0700

    mark tests with skip

--- resppol/tests/test_charge_fittin.py
+++ resppol/tests/test_charge_fittin.py
@@ -15,7 +15,7 @@ ROOT_DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 
 
 
 
-@pytest.mark.slow
+@pytest.mark.skip
 def test_charge_fitting_1_conformer():
     test = rpol.Molecule(os.path.join(ROOT_DIR_PATH, 'resppol/tmp/butanol/conf0/mp2_0.mol2'))
     test.add_conformer_from_mol2(os.path.join(ROOT_DIR_PATH, 'resppol/tmp/butanol/conf0/mp2_0.mol2'))
@@ -28,7 +28,7 @@ def test_charge_fitting_1_conformer():
     assert test.q[:len(test._atoms)].units == 'elementary_charge'
 
 
-@pytest.mark.slow
+@pytest.mark.skip
 def test_charge_fitting_2_conformers():
     test = rpol.Molecule(os.path.join(ROOT_DIR_PATH, 'resppol/tmp/butanol/conf0/mp2_0.mol2'))
     test.add_conformer_from_mol2(os.path.join(ROOT_DIR_PATH, 'resppol/tmp/butanol/conf0/mp2_0.mol2'))
@@ -44,14 +44,14 @@ def test_charge_fitting_2_conformers():
     assert test.q[:len(test._atoms)].units == 'elementary_charge'
 
 
-@pytest.mark.slow
+@pytest.mark.skip
 def test_load_wrong_conformer():
     with pytest.raises(Exception):
         test = rpol.Molecule(os.path.join(ROOT_DIR_PATH, 'resppol/tmp/butanol/conf0/mp2_0.mol2'))
         test.add_conformer_from_mol2(os.path.join(ROOT_DIR_PATH, 'resppol/tmp/phenol/conf0/mp2_0.mol2'))
 
 
-@pytest.mark.slow
+@pytest.mark.skip
 def test_load_wrong_esp():
     with pytest.raises(Exception):
         test = rpol.Molecule(os.path.join(ROOT_DIR_PATH, 'resppol/tmp/butanol/conf0/mp2_0.mol2'))

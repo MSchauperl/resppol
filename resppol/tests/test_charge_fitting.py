1862379899a075cb569ddd2e019cf7d31fd1642e
MSchauperl <schauperl.michael@gmail.com>
2019-05-07 11:16:09 -0700

    mark test as slow

--- resppol/tests/test_charge_fitting.py
+++ resppol/tests/test_charge_fitting.py
@@ -4,7 +4,7 @@ Test of the charge fitting process
 
 import pytest
 import resppol
-import resppol.rpol
+#import resppol.rpol
 import os
 
 ROOT_DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')

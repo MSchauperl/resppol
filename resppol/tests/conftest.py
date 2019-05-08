a3db52156bdb2f4b1a96eda2c09c72cbc9a62d39
MSchauperl <schauperl.michael@gmail.com>
2019-05-07 18:36:31 -0700

    added conftest.py

---
+++ resppol/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+def pytest_addoption(parser):
+    """Add the pytest command line option --runslow and --failwip.
+    If --runslow is not given, tests marked with pytest.mark.slow are
+    skipped.
+    If --failwip is not give, tests marked with pytest.mark.wip are
+    xfailed.
+    """
+    parser.addoption(
+        "--runslow", action="store_true", default=False, help="run slow tests"
+    )

baaee23f4e95bb886c6656c3449c59f9861fb0ca
MSchauperl <schauperl.michael@gmail.com>
2019-05-07 20:27:22 -0700

    removed skip slow

--- resppol/tests/test_charge_fitting.py
+++ resppol/tests/test_charge_fitting.py
@@ -17,7 +17,6 @@ ROOT_DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 
 
 
 
-@pytest.mark.slow
 def test_charge_fitting_1_conformer():
     test = resppol.rpol.Molecule(os.path.join(ROOT_DIR_PATH, 'resppol/data/test_data/butanol_0.mol2'))
     test.add_conformer_from_mol2(os.path.join(ROOT_DIR_PATH, 'resppol/data/test_data/butanol_0.mol2'))
@@ -31,6 +30,7 @@ def test_charge_fitting_1_conformer():
 
 
 
+@pytest.mark.slow
 def test_charge_fitting_1_conformer_resppol():
     datei = os.path.join(ROOT_DIR_PATH, 'resppol/tmp/butanol/conf0/mp2_0.mol2')
     test = resppol.rpol.Molecule(datei)

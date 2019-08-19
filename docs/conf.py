7866e6036e4cae51446958080d15f4304e4556b6
MSchauperl <schauperl.michael@gmail.com>
2019-08-19 15:46:45 -0700

    Enviroment for ReadTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../resppol/'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------

2593dd760c62d4ecb83188f6c191a9d6ae0ef344
MSchauperl <schauperl.michael@gmail.com>
2019-11-27 18:18:37 +0100

    1st part of G09 charge reader

---
+++ resppol/tests/test_utilities.py
@@ -0,0 +1,28 @@
+import pytest
+import os
+
+import resppol.utilities as util
+from openeye import oechem
+
+
+ROOT_DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')
+
+
+
+def test_read_mulliken_from_g09():
+    datei = os.path.join(ROOT_DIR_PATH, 'resppol/data/test_data/hfAM1Mulliken.log')
+    element_charge_array = util.read_mulliken_from_g09(datei)
+    assert len(element_charge_array) == 12
+    assert element_charge_array[3][0] == 'O'
+    assert element_charge_array[3][1] == '-0.333431'
+    assert element_charge_array[2][1] != '-0.333431'
+
+def test_load_g09_charges_to_oemol():
+    datei = os.path.join(ROOT_DIR_PATH, 'resppol/data/test_data/hfAM1Mulliken.log')
+    mol_datei = os.path.join(ROOT_DIR_PATH, 'resppol/data/test_data/butanol_0.mol2')
+    oemol = oechem.OEGraphMol()
+    ifs = oechem.oemolistream()
+    ifs.SetFormat(oechem.OEFormat_MOL2)
+    if ifs.open(mol_datei):
+        oechem.OEReadMolecule(ifs, oemol)
+    util.load_g09_charges_to_oemol(oemol,datei)
---
+++ resppol/utilities.py
@@ -0,0 +1,72 @@
+from openeye import oechem
+import sys
+
+
+atomic_number={'H':1,
+               'C':6,
+               'N':7,
+               'O':8}
+
+def read_mulliken_from_g09(g09_output):
+    """
+    Searches a gaussian output file for the Mulliken charge block and return the element and charge of every atom
+
+    :param g09_output: Gaussian output file
+    :return: List of [element,charge]
+    """
+    f=open(g09_output)
+    lines=f.readlines()
+    f.close()
+    element_charge_array=[]
+    for linenum,line in enumerate(lines):
+        print(line)
+        if 'Mulliken charges' in line:
+            while True:
+                if 'Sum' in lines[linenum+2]:
+                    break
+                else:
+                    num,element,charge=lines[linenum+2].split()
+                    element_charge_array.append([element,charge])
+                    linenum+=1
+            return(element_charge_array)
+
+
+
+def load_g09_charges_to_oemol(oemol, g09_output, mol2_output=None):
+    """
+    This functions takes a oemol and a link to a gaussian output file and matches the charges to the mol2 file.
+    Optional a new mol2 file with updated charges can be created with the optional keyword mol2_output.
+
+    :param oemol: openeye molecule object
+    :param g09_output: gaussian output file
+    :param mol2_output: mol2 output file with updated charges (optional)
+
+    :return: True if su
+    """
+
+    #1.) Read in Mulliken charge from a gaussian output file.
+    element_charges=read_mulliken_from_g09(g09_output)
+    #2.) Check if structure in gaussian output file is the same as in the mol2 file
+    # This comparison is just based on elements are therefore not ideal
+    for i,atom in enumerate(oemol.GetAtoms()):
+        if atom.GetAtomicNum == atomic_number[element_charges[i][0]]:
+            raise LookupError
+
+    #3.) Assign Charges to oemol
+
+    #4.) Write mol2 output if requested.
+    ofs = oechem.oemolostream()
+    oemol = oechem.OEGraphMol()
+
+    #Check if I can open the output file
+    if mol2_output is not None:
+        if ofs.open(mol2_output):
+            #If yes I write the mol2 file
+            ofs.SetFormat(oechem.OEFormat_MOL2)
+            oechem.OEWriteMolecule(ofs, oemol)
+
+        else:
+            # If not, I throw an exception
+            oechem.OEThrow.Fatal("Unable to create {}".format(mol2_output))
+
+
